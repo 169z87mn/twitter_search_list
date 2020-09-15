89232d8924e81e5dd8a71042009b454d8272a889
169z87mn <169z87mn@gmail.com>
2020-09-16 01:56:22 +0900

    fromでむりやりつなげた

--- twitter_search_list.py
+++ twitter_search_list.py
@@ -17,7 +17,8 @@ api = tweepy.API(auth)
 
 
 def lambda_handler(event, context):
-    query = create_query('Apple Watch')
+    query_str = 'Apple Watch'
+    query = add_from(query_str, LIST_ID)
     res = api.search(q=query)
     print(res)
     return {
@@ -27,3 +28,18 @@ def lambda_handler(event, context):
 
 def create_query(query: str):
     return query + ' list:' + LIST_ID
+
+
+def add_from(query: str, list_id: str):
+    members = get_list_members(list_id)
+    query += ' AND ' + ' OR '.join(['from:' + mem for mem in members])
+    return query[:500]
+
+
+def get_list_members(list_id: str):
+    members = []
+    res_members = api.list_members(list_id=list_id)
+
+    for mem in res_members:
+        members.append(mem.screen_name)
+    return members
